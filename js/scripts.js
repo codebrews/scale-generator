c2b944f91e965394ce2de5ad06d487d383eba626
codebrews <aaronc024@gmail.com>
2021-02-04 15:04:19 -0500

    Added initial content plus some experiments with css and js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "G#/Gb", "G", "G#/Gb"]
+var noteOne = notes[0]
+document.getElementsByClassName("one").innerText = `${noteOne}`
+console.log(notes[0])
