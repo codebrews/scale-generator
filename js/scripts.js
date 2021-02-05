02fb3bc2097e1bff4cc630bb2ba9a0bc8731dbbb
codebrews <aaronc024@gmail.com>
2021-02-05 15:00:09 -0500

    Created variables for note location ids and function for reordering display with key select element

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,66 @@
-var notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "G#/Gb", "G", "G#/Gb"];
+var notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"]; 
+const note1 = document.getElementById("one")
+const note2 = document.getElementById("two")
+const note3 = document.getElementById("three")
+const note4 = document.getElementById("four")
+const note5 = document.getElementById("five")
+const note6 = document.getElementById("six")
+const note7 = document.getElementById("seven")
+const note8 = document.getElementById("eight")
+const note9 = document.getElementById("nine")
+const note10 = document.getElementById("ten")
+const note11 = document.getElementById("eleven")
+const note12 = document.getElementById("twelve")
+const note13 = document.getElementById("thirteen")
 
-document.getElementById("one").innerHTML = notes[0];
-document.getElementById("two").innerHTML = notes[1];
-document.getElementById("three").innerHTML = notes[2];
-document.getElementById("four").innerHTML = notes[3];
-document.getElementById("five").innerHTML = notes[4];
-document.getElementById("six").innerHTML = notes[5];
-document.getElementById("seven").innerHTML = notes[6];
-document.getElementById("eight").innerHTML = notes[7];
-document.getElementById("nine").innerHTML = notes[8];
-document.getElementById("ten").innerHTML = notes[9];
-document.getElementById("eleven").innerHTML = notes[10];
-document.getElementById("twelve").innerHTML = notes[11];
-document.getElementById("thirteen").innerHTML = notes[0];
+note1.innerHTML = notes[0];
+note2.innerHTML = notes[1];
+note3.innerHTML = notes[2];
+note4.innerHTML = notes[3];
+note5.innerHTML = notes[4];
+note6.innerHTML = notes[5];
+note7.innerHTML = notes[6];
+note8.innerHTML = notes[7];
+note9.innerHTML = notes[8];
+note10.innerHTML = notes[9];
+note11.innerHTML = notes[10];
+note12.innerHTML = notes[11];
+note13.innerHTML = notes[0];
 
 function chooseKey(key){
-    if(notes[0] !== key)
+    while(notes[0] !== key){
         notes.push(notes.shift());
-
-        document.getElementById("one").innerHTML = notes[0];
-        document.getElementById("two").innerHTML = notes[1];
-        document.getElementById("three").innerHTML = notes[2];
-        document.getElementById("four").innerHTML = notes[3];
-        document.getElementById("five").innerHTML = notes[4];
-        document.getElementById("six").innerHTML = notes[5];
-        document.getElementById("seven").innerHTML = notes[6];
-        document.getElementById("eight").innerHTML = notes[7];
-        document.getElementById("nine").innerHTML = notes[8];
-        document.getElementById("ten").innerHTML = notes[9];
-        document.getElementById("eleven").innerHTML = notes[10];
-        document.getElementById("twelve").innerHTML = notes[11];
-        document.getElementById("thirteen").innerHTML = notes[0];
+    }
+    note1.innerHTML = notes[0];
+    note2.innerHTML = notes[1];
+    note3.innerHTML = notes[2];
+    note4.innerHTML = notes[3];
+    note5.innerHTML = notes[4];
+    note6.innerHTML = notes[5];
+    note7.innerHTML = notes[6];
+    note8.innerHTML = notes[7];
+    note9.innerHTML = notes[8];
+    note10.innerHTML = notes[9];
+    note11.innerHTML = notes[10];
+    note12.innerHTML = notes[11];
+    note13.innerHTML = notes[0];
 };
+
+const key = document.querySelector('#key');
+
+key.addEventListener('change', (event) => {
+    chooseKey(event.target.value);
+});
+
+// function chooseScale(scale){
+//     const scale = document.querySelector('#scale');
+//     if (scale === "major") {
+        
+//     }
+// }
+
+// const scale = document.querySelector('#scale');
+
+// scale.addEventListener('change', (event) => {
+//     chooseScale(event.target.value);
+// });
