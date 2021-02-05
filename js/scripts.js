5ea556c555822c66b2a94f8b19827f7c7003533b
codebrews <aaronc024@gmail.com>
2021-02-05 10:17:14 -0500

    Displayed notes in array on page and changed classes to ids for flex items in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 const notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "G#/Gb", "G", "G#/Gb"]
-var noteOne = notes[0]
-document.getElementsByClassName("one").innerText = `${noteOne}`
-console.log(notes[0])
+
+document.getElementById("one").innerHTML = notes[0]
+document.getElementById("two").innerHTML = notes[1]
+document.getElementById("three").innerHTML = notes[2]
+document.getElementById("four").innerHTML = notes[3]
+document.getElementById("five").innerHTML = notes[4]
+document.getElementById("six").innerHTML = notes[5]
+document.getElementById("seven").innerHTML = notes[6]
+document.getElementById("eight").innerHTML = notes[7]
+document.getElementById("nine").innerHTML = notes[8]
+document.getElementById("ten").innerHTML = notes[9]
+document.getElementById("eleven").innerHTML = notes[10]
+document.getElementById("twelve").innerHTML = notes[11]
+document.getElementById("thirteen").innerHTML = notes[0]
+
