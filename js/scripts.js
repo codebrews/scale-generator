699b540f08dc34c2038142709add1a71159e63f6
codebrews <aaronc024@gmail.com>
2021-02-06 10:47:36 -0500

    Finished functions for changing scales, conditions for four scales, and added class .note to divs to fix and enhance functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-var notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"]; 
+//  Array of notes to be manipulated and displayed on page
+var notes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"]; //an array of notes
+
+//  Divs to contain note names
 const note1 = document.getElementById("note1")
 const note2 = document.getElementById("note2")
 const note3 = document.getElementById("note3")
@@ -13,6 +16,28 @@ const note11 = document.getElementById("note11")
 const note12 = document.getElementById("note12")
 const note13 = document.getElementById("note13")
 
+//  Drop down menu elements
+const key = document.querySelector('#key');
+const scale = document.querySelector('#scale');
+
+//  Event listeners for drop down menus
+key.addEventListener('change', (event) => {
+    chooseKey(event.target.value);
+});
+
+scale.addEventListener('change', (event) => {
+    resetNotes();
+    chooseScale(event.target.value);
+});
+
+//  Functions!!!
+// function printNotes() {
+//     var noteDivs = document.getElementsByClassName("note");
+//     for(i = 0; i < noteDivs.length; i++){
+//         noteDivs[i].className = "note";
+//     }
+// }
+//  displays default order of note names from array to divs on page
 note1.innerHTML = notes[0];
 note2.innerHTML = notes[1];
 note3.innerHTML = notes[2];
@@ -27,11 +52,11 @@ note11.innerHTML = notes[10];
 note12.innerHTML = notes[11];
 note13.innerHTML = notes[0];
 
-function chooseKey(key){
-    while(notes[0] !== key){
+function chooseKey(key){                                 
+    while(notes[0] !== key){                //moves first note to end of array until chosen note or key is first in array            
         notes.push(notes.shift());
     }
-    note1.innerHTML = notes[0];
+    note1.innerHTML = notes[0];             //displays new order of notes on page
     note2.innerHTML = notes[1];
     note3.innerHTML = notes[2];
     note4.innerHTML = notes[3];
@@ -46,21 +71,44 @@ function chooseKey(key){
     note13.innerHTML = notes[0];
 };
 
-const key = document.querySelector('#key');
+function chooseScale(scale) {                            //fades display of notes that don't belong in scale based on user's scale choice
+    if (scale === "major") {
+        note2.classList.add("fade");
+        note4.classList.add("fade");
+        note7.classList.add("fade");
+        note9.classList.add("fade");
+        note11.classList.add("fade");
+    }
+    else if (scale === "minor") {
+        note2.classList.add("fade");
+        note5.classList.add("fade");
+        note7.classList.add("fade");
+        note10.classList.add("fade");
+        note12.classList.add("fade");
+    }
+    else if (scale === "majorPentatonic") {
+        note2.classList.add("fade");
+        note4.classList.add("fade");
+        note6.classList.add("fade");
+        note7.classList.add("fade");
+        note9.classList.add("fade");
+        note11.classList.add("fade");
+        note12.classList.add("fade");
+    }
+    else if (scale === "minorPentatonic") {
+        note2.classList.add("fade");
+        note3.classList.add("fade");
+        note5.classList.add("fade");
+        note7.classList.add("fade");
+        note9.classList.add("fade");
+        note10.classList.add("fade");
+        note12.classList.add("fade");
+    };
+}
 
-key.addEventListener('change', (event) => {
-    chooseKey(event.target.value);
-});
-
-// function chooseScale(scale){
-//     const scale = document.querySelector('#scale');
-//     if (scale === "major") {
-        
-//     }
-// }
-
-// const scale = document.querySelector('#scale');
-
-// scale.addEventListener('change', (event) => {
-//     chooseScale(event.target.value);
-// });
+function resetNotes(){
+    var noteDivs = document.getElementsByClassName("note");
+    for(i = 0; i < noteDivs.length; i++){
+        noteDivs[i].className = "note";
+    }   
+}
